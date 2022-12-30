54a0771a0a7f85aaecb2b8907786aea6d9f5db83
asus <asus@DESKTOP-J4576LJ>
2022-12-30 14:18:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace For1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите значение х=");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение n=");
+            byte n = byte.Parse(Console.ReadLine());
+            double s = 0;
+            for (int k = 1; k <= n; k++)
+                s += Math.Pow(x, k) * Math.Sin(k * (Math.PI / 4));
+            Console.WriteLine("s = {0}", s);
+            Console.WriteLine("Нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
